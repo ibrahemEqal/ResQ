3b9d2541cdddf8825d6f2f56d2f245217b24eb41
diamond <ibrahem452452@gmail.com>
2026-04-09 14:04:36 +0300

    resolve conflict

--- src/app/incident/components/ReportActions.tsx
+++ src/app/incident/components/ReportActions.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { styles } from '../styles';
-import { db } from './firebaseConfig';
-
 
 type ReportActionsProps = {
   onAssignResponder: () => void;
